31fa9be9649bf179593f521726824d5d8dbc4495
nonameeror <komangardanaputra01@gmail.com>
2026-01-06 13:19:31 +0800

    tampilan awal

--- soal2/jabawan2.py
+++ soal2/jabawan2.py
@@ -1,4 +1,8 @@
 
-print("======MENU PENGELOLAAN MAHASISWA=====")
-print("1.Tambah mahasiswa")
-print("cari")
+print("======MENU PENGELOLAAN MAHASISWA=====") 
+print("1.Tambah mahasiswa") 
+print("2.Tampilkan Semua Mahasiswa")
+print("3.Cari mahasiswa (NIM)")
+print("4.hapus Mahasiswa")
+print("5.keluar")   # inget save langsung save pake format .txt
+
